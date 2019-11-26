42768884a82c671d6cfb0fbba83f585032bdd75d
1357961823@qq.com <1357961823@qq.com>
2019-11-26 18:04:51 +0800

    发表文章

--- src/views/article/release.vue
+++ src/views/article/release.vue
@@ -4,24 +4,76 @@
 @import "~@/styles/mixin.scss";
 
 .inputs{
-  width: 260px;
+  width: 360px;
 }
+.describe {
+  color: #999;
+  margin-left: 10px;
+}
+.formItem {
+  width: 360px;
+}
+.demo-formData {
+.title {
+  margin: 15px 0;
+  font-weight: bold;
+  color: #333;
+}
+}
+.articleDetailBtn{
+  margin: 0 auto;
+}
+.articleAddWithDetail{
+  margin-top: 20px;
+  padding-top:20px ;
+  border: 1px solid #333;
+  border-radius: 8px;
+  width: 600px;
+  height: 300px ;
+}
+.articleAddWithDetail textarea{
+  border-radius: 8px;
+  width: 360px;
+  height: 100px;
+  resize:none;
+  outline: none !important;
+}
+.articleAddWithProduct{
+  margin-top: 20px;
+  padding-top:20px ;
+  border: 1px solid #333;
+  border-radius: 8px;
+  width: 600px;
+  height: 230px ;
+}
+.footer {
+  margin-top: 20px;
+@include flex-center;
+}
+  .imgArea{
+    border: 1px solid black;
+    width: 500px;
+    height: 4000px;
+  }
 </style>
 
 
 <template>
   <div>
     <el-form :model="formData" ref="formData" label-width="100px" class="demo-formData">
-      <el-form-item verify label="文章标题" prop="title">
+      <p class="title">文章基本信息</p>
+      <el-form-item verify label="文章封面图" prop="headImg">
         <el-upload
           :action="upImgUrl"
           :data="access_token"
           :show-file-list="false"
           :on-change="handleAvatarSuccess"
-          :before-upload="beforeAvatarUpload">
-          <img v-if="formData.imageUrl" :src="formData.imageUrl" class="avatar">
+          :before-upload="beforeAvatarUpload"
+          :on-success="uploadSuccess"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-          <el-input class="inputs none" v-model="formData.imageUrl"></el-input>
+          <el-input class="inputs none imgArea" v-model="imageUrl"></el-input>
         </el-upload>
       </el-form-item>
       <el-form-item verify label="文章标题" prop="title">
@@ -30,39 +82,67 @@
       <el-form-item verify label="文章副标题" prop="description">
         <el-input class="inputs" v-model="formData.description"></el-input>
       </el-form-item>
-      <el-form-item label="所属标签" verify prop="classif">
-        <el-checkbox-group v-model="sizeForm.classif">
-          <div class="classifBox" v-for="(item, index) in classif" :key="index">
-            <p>
-              {{item.parentName}}
-              <span>
-                <el-switch v-model="item.checkAll"  active-color="#13ce66" inactive-color="#f5f5f5" @change="handleCheckAllChange(index)">
-                </el-switch>
-                全选
-              </span>
-            </p>
-            <el-checkbox v-for="(l, i) in item.list" :key="i" :label="l.labelId">{{l.name}}</el-checkbox>
-          </div>
-        </el-checkbox-group>
-        <span class="describe">商品所属标签，可多选</span>
+      <el-form-item label="选择场景" verify prop="labelIds">
+        <el-select
+          class="formItem"
+          multiple
+          v-model="formData.sceneId"
+          placeholder="请选择场景"
+          @change="checkChange"
+        >
+          <el-option
+            v-for="item in scene"
+            :key="item.sceneId"
+            :label="item.name"
+            :value="item.sceneId"
+          ></el-option>
+        </el-select>
+        <span class="describe">文章场景，可多选</span>
       </el-form-item>
+      <div>
+        <p class="title">文章描述/添加产品</p>
+      </div>
+      <div class="articleDetailInfo">
+        <div class="articleDetailBtn">
+          <el-button type="primary"   @click="articleAddWithDetail">添加文章描述</el-button>
+          <el-button type="primary"  @click="articleAddWithProduct">添加相关产品</el-button>
+        </div>
+        <div class="articleAddWithDetail" v-for="(info,articleInfo) in articleInfo" :key='articleInfo'>
+          <el-form-item   label="描述小标题" prop="articleDetailTitle">
+            <el-input class="inputs" v-model="info.articleDetailTitle"></el-input>
+          </el-form-item>
+          <el-form-item   label="描述内容" prop="detailContent">
+            <textarea  class="inputs" v-model="info.detailContent"></textarea >
+          </el-form-item>
+          </el-form-item>
+          <el-form-item    label="排序" prop="sort">
+            <el-input class="inputs"   v-model="info.sort" ></el-input>
+            <span class="describe">排序数字(越小越前)</span>
+          </el-form-item>
+          <el-input class="inputs articleType" v-model="info.articleType" type="hidden" ></el-input>
+        </div>
+        <div class="articleAddWithProduct" v-for="(item,index) in articleProduct" :key="'product'+index">
+          <el-form-item  label="描述小标题"  prop="articleDetailTitle">
+            <el-input class="inputs" v-model="item.articleDetailTitle"></el-input>
+          </el-form-item>
+          <el-form-item label="选择产品" verify prop="labelIds">
+            <el-button class="btn" @click="chectProduct(index)">{{item.product.length>0?'已选'+item.product.length+'件商品':'选择商品'}}</el-button>
+            <span class="describe">选择产品</span>
+          </el-form-item>
+          <el-form-item   label="排序"  prop="sort">
+            <el-input class="inputs"  v-model="item.sort" ></el-input>
+            <span class="describe">排序数字(越小越前)</span>
+          </el-form-item>
+          <el-input class="inputs articleType" v-model="item.articleType" type="hidden" ></el-input>
+        </div>
+      </div>
+      <div class="footer">
+        <el-button type="primary" @click="releaseData">确认发布</el-button>
+      </div>
     </el-form>
-    <!-- <el-upload
-            class="upload-demo"
-            :action="upImgUrl"
-            :data="access_token"
-            :file-list="detailList"
-            :show-file-list="false"
-            :before-upload="beforeAvatarUpload"
-            multiple
-            :on-change="detailListenChange"
-            list-type="picture"
-          >
-            <p class="flex-center upImtBtn">
-              <i class="el-icon-plus"></i>
-            </p>
-            <p>上传商品图片</p>
-          </el-upload> -->
+    <el-dialog title="选择商品" :visible.sync="productCode" width="90%">
+      <product-list v-if="productCode" checkItem @selectData="selectData" :selectArr="selects"></product-list>
+    </el-dialog>
   </div>
 </template>
 
@@ -70,31 +150,119 @@
 <script>
 import {
   productLabel,
-  produckTabel
+  produckTabel,
+  produckList,
+  productScenelList,
+  postArticleRelease
 } from "@/api/table";
+import productList from "@/views/product/list";
 
 export default {
   name: "ARTICLE_RELEASE",
+  components:{
+    productList
+  },
   data() {
     return {
       formData:{
-        memberId: '1',
-        headImg:'',
-        classif: []
+        labelIds:"",
+        headImg:"",
+        title:"",
+        description:"",
+        memberId:"1",
+        sceneId:'',
+        detailsJson:[{
+          articleDetailTitle:"",
+          detailContent:"",
+          articleType:"",
+          sort:0
+        }]
       },
+      scene: [],
+      product: [],
       upImgUrl: process.env[this.$base] + "/medias/image/upload",
       access_token: {
         access_token: this.$store.getters.token
       },
       imageUrl: '',
-      classif: []
+      classif: [],
+      articleInfo: [],
+      articleProduct: [],
+      selects:[],
+      categoryIndex:"",
+      productCode:false
     };
   },
   created() {
+    productScenelList()
+      .then(res => {
+      this.scene = res.data;
+    //解析表单数据
+    }),
+    produckList()
+      .then(res => {
+      this.product = res.data.list;
+    //解析表单数据
+  })
   },
   methods: {
-    handleAvatarSuccess(){
-
+    //商品筛选：
+    dataFilter(name){
+      this.name = name;
+      console.log(this.name);
+      if(name){
+        this.options = this.product.filter((item) => {
+            if(!!~item.name.indexOf(name) || !!~item.name.toUpperCase().indexOf(name.toUpperCase())){
+              return true;
+            }
+          })
+      }else { //val为空时，还原数组
+        this.options = this.product;
+      }
+    },
+    articleAddWithDetail(){
+      let info={
+        articleDetailTitle:"",
+        detailContent:"",
+        articleType:"ARTICLE",
+        sort:""
+      };
+      this.articleInfo.push(info);
+    },
+    articleAddWithProduct(){
+      let item={
+        articleDetailTitle:"",
+        detailContent:"",
+        articleType:"PRODUCT",
+        sort:"",
+        product:[]
+      };
+      this.articleProduct.push(item);
+    },
+    selectData(val){
+      if(val.length>1){
+        this.$message.error('一次选择一个产品');
+        return
+      }
+      this.productCode = false;
+      this.articleProduct[this.categoryIndex].product = val;
+      this.articleProduct[this.categoryIndex].detailContent = JSON.parse(JSON.stringify(val))[0].productId;
+    },
+    chectProduct(i){
+      this.selects = JSON.parse(JSON.stringify(this.articleProduct[i].product));
+      this.categoryIndex = i;
+      this.productCode=true
+    },
+    handleAvatarSuccess(file, fileList){
+    },
+    uploadSuccess(response,file,fileList){
+      if(response.code=='200'){
+        this.formData.headImg = response.data;
+        this.imageUrl = response.data
+      }
+    },
+    checkChange(){
+      console.log("data===="+this.formData.sceneId);
     },
     beforeAvatarUpload(file) {
       var type = "image/jpg,image/jpeg,image/png,image/gif";
@@ -109,49 +277,62 @@ export default {
       }
       return isJPG && isLt2M;
     },
-    handleCheckAllChange(i){
-      this.classif[i].list.forEach(e => {
-        var index = this.sizeForm.classif.indexOf(e.labelId)
-        if(this.classif[i].checkAll){
-          if(index == -1){
-            this.sizeForm.classif.push(e.labelId)
-          }
-        }else{
-          if(index != -1){
-            this.sizeForm.classif.splice(index,1)
-          }
+    releaseData(){
+      //发布文章请求参数组装
+      let data = {
+        labelIds: "",
+        headImg: this.formData.headImg,
+        title: this.formData.title,
+        description: this.formData.description,
+        memberId: this.formData.memberId,
+        detailsJson: ""
+      };
+      //labelIds转为1,2,3的格式
+      data.labelIds = this.formData.sceneId.join(",");
+      //detailsJson数组转为字符串
+//            "[{"articleDetailTitle": "articleDetailTitle", "detailContent": "detailContent", "articleType": "articleType", "sort": "0"},]"
+      //组装数据
+      let arr = [];
+      this.articleInfo.forEach(item  => {
+        let article = {
+          articleDetailTitle:item.articleDetailTitle,
+          detailContent:item.detailContent,
+          articleType:item.articleType,
+          sort:item.sort
         }
-      });
-    },
-    getClassif(val) {
-      var list = [];
-      var code = 0;
-      var self = this;
-      val.forEach((e,i) => {
-        produckTabel({ sceneId: e }).then(result => {
-          var parentName =  ''
-          try {
-            parentName = result.data[0].sceneName
-          } catch (error) {}
-          var obj = {
-            list : result.data,
-            parentName: parentName,
-            checkAll: false
-          }
-          list.push(obj);
-          code++;
-          if (code == val.length) {
-            successFun();
-          }
+        arr.push(article)
+    })
+      this.articleProduct.forEach(item  => {
+        let article = {
+          articleDetailTitle:item.articleDetailTitle,
+          detailContent:item.detailContent,
+          articleType:item.articleType,
+          sort:item.sort
+        }
+        arr.push(article)
+    })
+      data.detailsJson = JSON.stringify(arr);
+      console.log(data);
+      return false;
+      postArticleRelease(data)
+        .then(result => {
+        if (result.code == 200) {
+        this.$message({
+          message: "发布成功",
+          type: "success"
         });
-      });
-      if(!val||val.length==0){
-        this.classif = []
+        location.reload();
+        this.updataInfo = 2;
+      } else {
+        this.$message.error(result.description);
+        this.updataInfo = 2;
       }
-      function successFun() {
-        self.classif = list;
-      }
-    },
+    })
+    .catch(err => {
+        this.$message.error("网络错误");
+      this.updataInfo = 2;
+    });
+    }
   }
 };
 </script>
