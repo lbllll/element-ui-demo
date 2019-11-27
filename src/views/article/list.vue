548f12d8ca7a927e54b5923bba3946e81218865f
1357961823@qq.com <1357961823@qq.com>
2019-11-27 18:07:22 +0800

    文章列表

--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -3,45 +3,71 @@
 
 
 <style lang="scss" scoped>
-@import "~@/styles/mixin.scss";
+  @import "~@/styles/mixin.scss";
 </style>
 
 
 <template>
-  <div class="bodyBox" id>
-    <el-table :data="tableData" border :header-cell-style="{background:'#afafaf',color:'#606266'}">
-      <el-table-column label="文章上下架" width="100" align="center">
+  <div class="bodyBox" id="">
+    <el-table
+      :data="tableData"
+      border
+      :header-cell-style="{background:'#afafaf',color:'#606266'}"
+    >
+      <el-table-column
+        label="文章标题"  width="400" align="center">
         <template slot-scope="scope">
-          <el-switch
-            v-model="scope.row.status"
-            @change="changeShow(scope.row)"
-            active-color="#13ce66"
-            inactive-color="#ff4949"
-            active-value="PUTAWAY"
-            inactive-value="SOLD_OUT">
-          </el-switch>
+          <span>{{ scope.row.title}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="文章标题" width="400" prop="title" align="center"></el-table-column>
-      <el-table-column label="标签" width="300" prop="labels" align="center"></el-table-column>
-      <el-table-column label="封面图" width="120" align="center">
+      <el-table-column
+        label="标签"  width="300" align="center">
         <template slot-scope="scope">
-          <span>
-            <img :src="scope.row.headImg" width="100" height="100" />
-          </span>
+          <span>{{ scope.row.labels}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="列表图" width="120" align="center">
+
+      <!--      <el-table-column
+              label="标签id">
+              <template slot-scope="scope">
+                <span>{{ scope.row.labelIds}}</span>
+              </template>
+            </el-table-column>-->
+
+      <el-table-column
+        label="封面图" width="120" align="center" >
         <template slot-scope="scope">
-          <span>
-            <img :src="scope.row.listImg" width="100" height="100" />
-          </span>
+          <span><img :src="scope.row.headImg" width="100" height="100"></span>
         </template>
       </el-table-column>
-      <el-table-column label="副标题" width="400" prop="description" align="center"></el-table-column>
-      <el-table-column label="点赞数" width="100" prop="likeCount" align="center"></el-table-column>
-      <el-table-column label="分享数" width="100" prop="shareCount" align="center"></el-table-column>
-      <el-table-column label="商户id" width="100" prop="memberId" align="center"></el-table-column>
+
+      <el-table-column
+        label="副标题" width="400" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.description}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="点赞数" width="100" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.likeCount}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="分享数" width="100" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.shareCount}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="商户id" width="100" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.memberId}}</span>
+        </template>
+      </el-table-column>
     </el-table>
     <el-pagination
       @current-change="handleCurrentChange"
@@ -56,66 +82,54 @@
 
 
 <script>
-import { articleList,postApi } from "@/api/table";
-export default {
-  name: "ARTICLE_LIST",
-  data() {
-    return {
-      data: {
-        title: "", // 文章标题
-        labelIds: "", // 标签id
-        memberId: "", // 商户id
-        page: 1,
-        size: 10
-      },
-      tableData: [],
-      checkAll: false,
-      isIndeterminate: false,
-      count: 0,
-      multipleSelection: [],
-      articleLists: []
-    };
-  },
-  props: {
-    checkItem: Boolean,
-    selectArr: Array
-  },
-  created() {},
-  methods: {
-    init() {
-      this.data.page = 0;
-      this.data.size = 10;
-      articleList(this.data).then(result => {
-        this.count = result.data.count;
+  import {
+    articleList
+  } from "@/api/table";
+  export default {
+    name: "ARTICLE_LIST",
+    data() {
+      return {
+        data: {
+          title: "", // 文章标题
+          labelIds: "", // 标签id
+          memberId: "", // 商户id
+          page: 1,
+          size: 10
+        },
+        tableData: [],
+        checkAll: false,
+        isIndeterminate: false,
+        count: 0,
+        multipleSelection: [],
+        articleLists:[]
+      };
+    },
+    props: {
+      checkItem: Boolean,
+      selectArr: Array
+    },
+    created() {
+
+    },
+    methods: {
+      init() {
+        this.data.page=0;
+        this.data.size=10;
+        articleList(this.data).then(result => {
+          this.count = result.data.count;
         this.tableData = result.data.articleLists;
-      });
+      })
+      },
+      handleCurrentChange(val) {
+        this.data.page = val;
+        this.getList();
+        console.log(`当前页: ${val}`);
+      },
     },
-    handleCurrentChange(val) {
-      this.data.page = val;
-      this.getList();
-      console.log(`当前页: ${val}`);
-    },
-    changeShow(row){
-      var url = ''
-      if(row.status=='PUTAWAY'){
-        url = '/article/info/putAway'
-      }else{
-        url = '/article/info/soldOut'
-      }
-      postApi({articleId:row.articleId},url).then((res) => {
-        this.$message({
-          message:res.data,
-          type: 'success'
-        })
-      }).catch((err) => {
-        
-      });
+    mounted(){
+      this.init()
     }
-  },
-  mounted() {
-    this.init();
   }
-};
 </script>
 
 
