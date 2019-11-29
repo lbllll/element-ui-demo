84300fb267284557352d3728a02537b81330e733
1357961823@qq.com <1357961823@qq.com>
2019-11-29 16:07:54 +0800

    文章列表条件筛选

--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -4,12 +4,80 @@
 
 <style lang="scss" scoped>
 @import "~@/styles/mixin.scss";
+
+.tableBox {
+  margin-top: 10px;
+}
+.searchBox {
+  height: 50px;
+  width: 100%;
+  margin-top: 20px;
+  padding: 0 20px;
+  border-radius: 4px;
+  .radio{
+    float: left;
+    width: 300px;
+  }
+  .title,.scene,.search{
+    margin-left: 45px;
+    float: left;
+    height: 50px;
+  }
+}
 </style>
 
 
 <template>
   <div class="bodyBox" id>
-    <el-table :data="tableData" border :header-cell-style="{background:'#afafaf',color:'#606266'}">
+    <div class="listType">
+      <!--状态查询 -->
+      <el-form :model="data" ref="searchs" class="searchBox">
+        <div class="radio">
+          <el-radio-group
+            v-model="data.status"
+            @change="init"
+            size="small"
+          >
+            <el-radio label border>全部</el-radio>
+            <el-radio label="PUTAWAY" border>上架</el-radio>
+            <el-radio label="SOLD_OUT" border>下架</el-radio>
+          </el-radio-group>
+        </div>
+        <div class="title">
+          <el-form-item prop="title"  can-be-empty>
+            <el-input class="inputs" placeholder="请输入标题关键字" size="mini" v-model="data.title"></el-input>
+          </el-form-item>
+        </div>
+        <div class="scene">
+          <el-form-item  prop="labelIds">
+            <el-select
+              class="data"
+              v-model="data.labelIds"
+              placeholder="请选择文章包含场景"
+              size="small"
+            >
+              <el-option value="">全部</el-option>
+              <el-option
+                v-for="item in scene"
+                :key="item.sceneId"
+                :label="item.name"
+                :value="item.sceneId"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </div>
+        <div class="search">
+          <el-button type="primary"  size="small" @click="searchs()">搜索</el-button>
+        </div>
+      </el-form>
+    </div>
+    <el-table
+      class="tableBox"
+      :data="tableData"
+      border
+      :header-cell-style="{background:'#afafaf',color:'#606266'}"
+      :row-key="getRowKeys"
+    >
       <el-table-column label="封面图" width="120" align="center">
         <template slot-scope="scope">
           <span>
@@ -26,7 +94,7 @@
       </el-table-column>
       <el-table-column label="文章标题"  prop="title" align="center"></el-table-column>
       <el-table-column label="副标题"  prop="description" align="center"></el-table-column>
-      <el-table-column label="标签" width="250" prop="labels" align="center"></el-table-column>
+      <el-table-column label="场景" width="250" prop="labels" align="center"></el-table-column>
       <el-table-column label="文章上下架" width="100" align="center">
         <template slot-scope="scope">
           <el-switch
@@ -67,19 +135,24 @@
 import {
   articleList,
   postApi,
-  articleDel
+  articleDel,
+  productScenelList
 } from "@/api/table";
 export default {
   name: "ARTICLE_LIST",
   data() {
     return {
       data: {
+        listType: "", // 顶部按钮
         title: "", // 文章标题
         labelIds: "", // 标签id
         memberId: "", // 商户id
+        status:"",//状态
         page: 1,
         size: 10
       },
+      scenes:'',
+      scene:[],
       tableData: [],
       checkAll: false,
       isIndeterminate: false,
@@ -92,20 +165,44 @@ export default {
     checkItem: Boolean,
     selectArr: Array
   },
-  created() {},
+  created() {
+    productScenelList().then(res => {
+      this.scene = res.data;
+    //解析表单数据
+  })
+    this.init()
+  },
   methods: {
     init() {
-      this.data.page = 0;
-      this.data.size = 10;
-      articleList(this.data).then(result => {
+      window.sessionStorage.setItem('articleSearch',JSON.stringify(this.data));
+      let data = JSON.parse(JSON.stringify(this.data));
+      data.page --;
+      articleList(data).then(result => {
         this.count = result.data.count;
         this.tableData = result.data.articleLists;
-      });
+      this.tableData.forEach(item => {
+        item.labels= item.labels.toString().replace(/,/g," | ")
+      })
+    });
     },
     handleCurrentChange(val) {
       this.data.page = val;
-      this.getList();
+      this.init();
       console.log(`当前页: ${val}`);
+    },
+    getRowKeys(row) {
+      // 给表格每行增加一个唯一 标识，用作切换分页后保留被选中行的信息
+      return row.articleId;
+    },
+    searchs() {
+      this.$refs.searchs.validate(valid => {
+        if (valid) {
+          this.init();
+        } else {
+          console.log("error submit!!");
+      return false;
+    }
+    });
     },
     changeShow(row){
       var url = ''
